4764d1e81de1d524f8d3fd312d0b59b55da38f51
eddy556 <eddy556@gmail.com>
2015-01-28 23:47:46 +0000

    Added fadein for helper

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a message:");
+            string input = Console.ReadLine();
+            Data d = new Data
+            {
+                ID = 5,
+                Message = input,
+                CurrentDateTime = System.DateTime.Now
+            };
+            Console.WriteLine("You entered: " + d);
+        }
+    }
+
+    public class Data
+    {
+        public int ID { get; set; }
+        public string Message { get; set; }
+        public DateTime CurrentDateTime { get; set; }
+
+
+        public override string ToString()
+        {
+            Console.ReadLine();
+            return string.Format("ID:{0} {1} at {2}", ID, Message,
+
+CurrentDateTime.ToLongTimeString());
+        }
+    }
+}
